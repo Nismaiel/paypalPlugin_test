760d45b428be7e62a3dbb1b875ab0e9fe7a9a7a2
Nismaiel <44040428+Nismaiel@users.noreply.github.com>
2020-08-17 01:27:42 +0200

    main initiated

--- lib/main.dart
+++ lib/main.dart
@@ -5,11 +5,45 @@ void main() {
   runApp(MyApp());
 }
 
-class MyApp extends StatelessWidget {
+class MyApp extends StatefulWidget {
   // This widget is the root of your application.
+  @override
+  _MyAppState createState() => _MyAppState();
+}
+
+class _MyAppState extends State<MyApp> {
+  final GlobalKey<ScaffoldState> _scaffoldKey = new GlobalKey<ScaffoldState>();
+
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Paybal',
-      home: Home()
-    );}}
+        title: 'Paybal',
+        home: Scaffold(
+          key: _scaffoldKey,
+          appBar: AppBar(
+            title: Text(
+              'Paypal Demo',
+              style: TextStyle(color: Colors.white),
+            ),
+            centerTitle: true,
+          ),
+        body: Container(
+          width: MediaQuery.of(context).size.width,
+          child: Column(
+            crossAxisAlignment:CrossAxisAlignment.center ,
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              //making the payment
+              RaisedButton(onPressed: (){
+                Navigator.push(context, MaterialPageRoute(builder: (ctx)=>Home(onFinish: (number)async{
+                  print('order id: '+number );
+                },)));
+              },child: Text('pay with Paypal'),
+
+              )
+            ],
+          ),
+        ),
+        ));
+  }
+}
